38d1db645c006f27ce632ba00317f2a71c2f3169
Alex Hua <huazifan@gmail.com>
2023-03-13 13:40:42 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 # a developer codeSigningCertificate is required to sign the app
 codeSigningCertificate=""
 
+rm -r ./AppOcr.app
+rm -r ./AppOcr.tar.gz
+
 mkdir -p ./AppOcr.app/Contents/MacOS/
 mkdir -p ./AppOcr.app/Contents/Resources/
 
@@ -10,3 +13,5 @@ cp main ./AppOcr.app/Contents/MacOS/AppOcr
 cp ./Info.plist ./AppOcr.app/Contents/
 cp ./Icon.icns ./AppOcr.app/Contents/Resources/
 codesign -s $codeSigningCertificate -f ./AppOcr.app/
+
+tar -czvf ./AppOcr.tar.gz ./AppOcr.app
